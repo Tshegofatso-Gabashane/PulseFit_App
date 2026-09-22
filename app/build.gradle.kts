4187ba28d6e462ef6a11c38ee685cb8f41370430
ST10461244 <st10461244@rcconnect.edu.za>
2026-09-22 22:05:00 +0200

    exercises page fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,7 @@ dependencies {
 
     implementation("com.google.android.gms:play-services-maps:19.2.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
